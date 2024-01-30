6848f697ec93045b1c2b1c59bcaa84b9cb2e37e3
hridoythebest <hridoythebest@gmail.com>
2024-01-30 08:43:45 +0600

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import Home from './components/Home';
 import Services from "./components/Services";
+import About from "./components/About";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
    <Navbar />
    <Home/>
    <Services/>
+   <About/>
    </>
   )
 }
