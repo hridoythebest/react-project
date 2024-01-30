4a6462c432e076159f0cbf6cccaf9971c87abb2f
hridoythebest <hridoythebest@gmail.com>
2024-01-30 08:48:16 +0600

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,19 @@ import about from '../assets/about.png'
 
 const About = () => {
     return (
-        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8'>
-            <div className='md:w-11/12 mx-auto flex flex-col md:flex-row lg:flex-row justify-between items-center gap-12'>
-                <img src={about} alt="" />
-            </div>
+        <div>
+            <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8 flex'>
+                <div className='md:w-6/12 mx-auto flex flex-col md:flex-row lg:flex-row justify-between items-center gap-12'>
+                    <img src={about} alt="" />
+                </div>
 
-            <div className='md:w-3/5 mx-auto'>
-                <h2 className='text-4xl text-neutralGray font-semibold mb-4 md:w-4/5'>The unseen of spending three years at Pixelgrade</h2>
-                <p className='md:w-3/4 text-sm text-neutralGray mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
-                <button className='px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralGray transition-all duration-300 hover:-translate-y-4'>Learn More</button>
+                <div className='md:w-3/5 mx-auto'>
+                    <h2 className='text-4xl text-neutralGray font-semibold mb-4 md:w-4/5'>The unseen of spending three years at Pixelgrade</h2>
+                    <p className='md:w-3/4 text-sm text-neutralGray mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                    <button className='px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralGray transition-all duration-300 hover:-translate-y-4'>Learn More</button>
+                </div>
+
             </div>
-           
         </div>
     );
 };
